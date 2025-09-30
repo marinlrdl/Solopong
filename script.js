2df6e54fdb5e906f71f329acee4e03db6cf02997
Marin Lerondel--Touzé <marin.lerondel-touze@etu.unicaen.fr>
2025-09-30 17:23:44 +0200

    Dessin dans une fonction 'draw()'

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const canvas = document.getElementById('pong');
 const ctx = canvas.getContext('2d');
+let startTime;
+let ball = { x: canvas.width / 2, y: canvas.height / 2, radius: 20, speed: 5, dx: 0, dy: 0 };
+let paddle = { x: 10, y: canvas.height - 40, width: 100, height: 10 };
+let score = document.getElementById('score');
+let reset = document.getElementById('reset-button');
 
-ctx.fillStyle = 'white';
-ctx.fillRect(canvas.width / 2 - 40, canvas.height - 20, 80, 10);
+function draw() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
 
-ctx.beginPath();
-ctx.arc(50, 50, 15, 0, Math.PI * 2);
-ctx.fillStyle = 'white';
-ctx.fill();
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+}
+
+draw();
